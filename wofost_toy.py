ba5d7956a56f243418efa831bbd384f79305aaf2
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2023-11-08 13:45:31 +0100

    Add biomass

--- wofost_toy.py
+++ wofost_toy.py
@@ -2,7 +2,7 @@
 Playing around with the PCSE implementation of WOFOST.
 """
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, dates as mdates
 import pandas as pd
 
 import pcse
@@ -21,9 +21,20 @@ summary_output = wofost_object.get_summary_output()
 msg = "Reached maturity at {DOM} with total biomass {TAGP} kg/ha and a yield of {TWSO} kg/ha."
 print(msg.format(**summary_output[0]))
 
-plt.plot(df["day"], df["LAI"])
-plt.title("WOFOST growth curve - toy example")
-plt.xlabel("Time")
-plt.ylabel("Leaf area index (LAI) [m²/m²]")
+fig, ax1 = plt.subplots()
+ax1.plot(df["day"], df["TWSO"], color="black")
+ax1.plot(df["day"], df["TAGP"], color="red")
+ax1.set_title("WOFOST growth curve - toy example \nWater-limited winter wheat")
+ax1.set_xlabel("Time")
+ax1.set_ylabel("Biomass, Yield [kg/ha]")
+ax1.set_ylim(ymin=0)
+# ax1.set_xlim(df["day"].min(), df["day"].max())
+
+ax2 = ax1.twinx()
+ax2.plot(df["day"], df["LAI"], color="blue")
+ax2.set_ylabel("Leaf area index (LAI) [m²/m²]", color="blue")
+ax2.tick_params(axis="y", colors="blue")
+ax2.set_ylim(ymin=0)
+
 plt.show()
 plt.close()
