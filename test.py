73516d8e3fb7f270d820220f66b468f5075d7228
Olivier Burggraaff <olivierburggraaff@gmail.com>
2023-12-19 14:33:59 +0100

    Use simpler multiprocessing loop

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-# https://stackoverflow.com/a/41921948/2229219
-from multiprocessing import Pool
+"""
+Testing multiprocessing + tqdm
+https://stackoverflow.com/q/41920124/2229219
+"""
+
+from multiprocessing.dummy import Pool
+import tqdm
 import time
-from tqdm import *
 
 def _foo(my_number):
-   square = my_number * my_number
-   return square
+    square = my_number * my_number
+    return square
 
 if __name__ == '__main__':
-    with Pool(processes=8) as p:
-        max_ = 300000
-        with tqdm(total=max_) as pbar:
-            for _ in p.imap_unordered(_foo, range(0, max_)):
-                pbar.update()
+    max = 3000000
+    with Pool() as p:
+        r = list(tqdm.tqdm(p.map(_foo, range(max)), total=max))
