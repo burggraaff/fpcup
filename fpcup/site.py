741c3b4cf5f4c4ab2a765a5b16c938f5a1f09032
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-01-03 15:46:46 +0100

    Generate linearly spaced coordinates

--- fpcup/site.py
+++ fpcup/site.py
@@ -10,7 +10,7 @@ from pcse.util import WOFOST72SiteDataProvider, WOFOST80SiteDataProvider
 
 def grid_coordinate_range(latitude: tuple[Number], longitude: tuple[Number]) -> list[tuple[float]]:
     """
-    Generate all pairs of latitude/longitude coordinates.
+    Generate all pairs of latitude/longitude coordinates in given latitude/longitude ranges.
     Inputs should be tuples that can be passed to np.arange.
     The output is given as a list so it can be iterated over multiple times.
 
@@ -19,6 +19,23 @@ def grid_coordinate_range(latitude: tuple[Number], longitude: tuple[Number]) -> 
     """
     latitude_range = np.arange(*latitude)
     longitude_range = np.arange(*longitude)
+    coordinates = product(latitude_range, longitude_range)
+    coordinates = list(coordinates)
+    return coordinates
+
+def grid_coordinate_linspace(latitude: tuple[Number], longitude: tuple[Number], n: int) -> list[tuple[float]]:
+    """
+    Generate n pairs of latitude/longitude coordinates in a given area bound by latitude/longitude ranges.
+    Inputs should be tuples of (min, max) latitude/longitude.
+    The output is given as a list so it can be iterated over multiple times.
+    Note: the true size of the output may be smaller than n due to rounding.
+
+    Example:
+        coords = grid_coordinate_range(latitudes=(50, 52), longitudes=(5, 10), n=1000)
+    """
+    n_each = int(np.sqrt(n))
+    latitude_range = np.linspace(*latitude, n_each)
+    longitude_range = np.linspace(*longitude, n_each)
     coordinates = product(latitude_range, longitude_range)
     coordinates = list(coordinates)
     return coordinates
