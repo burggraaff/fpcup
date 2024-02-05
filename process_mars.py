093eb1cddda760bb5227d2a0d6a3af1709e76459
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-02-05 18:49:37 +0100

    Regrid

--- process_mars.py
+++ process_mars.py
@@ -9,9 +9,17 @@ The following conversions need to take place to become PCSE-compatible:
 from datetime import date
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
+import geopandas as gpd
+
+from scipy.interpolate import griddata
+
+from matplotlib import pyplot as plt
 
 from pcse.util import wind10to2
+
+from fpcup.province import nl, CRS_AMERSFOORT
 
 filename = Path("data/meteo/NL-weather_ver2024_01_28946_162168065.csv")
 
@@ -26,3 +34,38 @@ convert_all = {"DAY": convert_day, "WINDSPEED": convert_windspeed, "PRECIPITATIO
 # Read the data
 data = pd.read_csv(filename, sep=";", converters=convert_all)
 print(f"Read data from {filename.absolute()}")
+
+# Sort by coordinates
+data.sort_values(["LONGITUDE", "LATITUDE"], inplace=True)
+
+# Convert to GeoPandas
+data = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data["LONGITUDE"], data["LATITUDE"], crs="WGS84"))
+
+# Regrid: definitions
+data.to_crs(CRS_AMERSFOORT, inplace=True)
+xmin, xmax, ymin, ymax = data.total_bounds
+cellsize = 250  # m
+newx = np.arange(xmin, xmax+cellsize, cellsize)
+newy = np.arange(ymin, ymax+cellsize, cellsize)
+newx, newy = np.meshgrid(newx, newy)
+
+# Regrid: example
+key = "RADIATION"
+day = data["DAY"].iloc[1]
+data_day = data.loc[data["DAY"] == day]
+
+gridded = griddata((data_day["geometry"].x, data_day["geometry"].y), data_day[key], (newx, newy), method="cubic")
+
+# gridded_df = gpd.GeoDataFrame(gridded, geometry=gpd.points_from_xy(newx, newy, crs=CRS_AMERSFOORT))
+
+# Regrid: plot
+vmin, vmax = np.nanmin(gridded), np.nanmax(gridded)
+fig, ax = plt.subplots(1, 1, figsize=(5,5))
+im = ax.imshow(gridded, origin="lower", extent=(newx.min(), newx.max(), newy.min(), newy.max()), vmin=vmin, vmax=vmax)
+data_day.plot(key, ax=ax, edgecolor="black", vmin=vmin, vmax=vmax)
+nl.boundary.plot(ax=ax, color="black", lw=1)
+
+fig.colorbar(im, ax=ax, label=key)
+
+plt.show()
+plt.close()
