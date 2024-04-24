632559a32366650f185a57b2bfea9d176e59d9d5
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-04-24 23:55:36 +0200

    Return as tensors

--- test/nn.py
+++ test/nn.py
@@ -11,7 +11,6 @@ from torch import nn, optim, tensor, Tensor
 from torch.utils.data import DataLoader, Dataset
 
 import fpcup
-from fpcup.io import Path
 from fpcup.typing import PathOrStr
 
 
@@ -42,15 +41,22 @@ class PCSEEnsembleDataset(Dataset):
     def __getitem__(self, i: int) -> tuple[Tensor, Tensor]:
         # Load input data
         input_filename = self.input_files[i]
-        input_data = fpcup.io.read_gpd(input_filename).iloc[0]
-        latitude, longitude = input_data["geometry"].y, input_data["geometry"].x
-        sowyear, sowdoy = 1
+        input_data = fpcup.model.InputSummary.from_file(input_filename).iloc[0]
+        sowyear = input_data["DOS"].year
+        sowdoy = input_data["DOS"].day_of_year
+
+        input_data = input_data[["latitude", "longitude", "WAV", "RDMSOL"]].to_list() + [sowyear, sowdoy]
 
         # Load summary data
         summary_filename = self.summary_files[i]
-        summary_data = fpcup.io.read_gpd(summary_filename).iloc[0]
+        summary_data = fpcup.model.Summary.from_file(summary_filename).iloc[0]
 
-        return input_data, summary_data
+        matyear = summary_data["DOM"].year
+        matdoy = summary_data["DOM"].day_of_year
+
+        summary_data = summary_data[["DVS", "LAIMAX", "TAGP", "TWSO"]].to_list() + [matyear, matdoy]
+
+        return tensor(input_data), tensor(summary_data)
 
 
 ### Parse command line arguments
@@ -91,32 +97,3 @@ if __name__ == "__main__":
 
     ### SETUP
     data = PCSEEnsembleDataset(args.output_dir)
-    raise Exception
-
-    # Load the ensemble summary
-    inputsummary, summary = fpcup.io.load_ensemble_summary_from_folder(args.output_dir)
-    if args.verbose:
-        print(f"Loaded input summary, summary from {args.output_dir.absolute()}")
-
-    # Join the input/output tables
-    inputsummary.drop(columns=["geometry"], inplace=True)
-    summary = summary.join(inputsummary, validate="1:1")
-    if args.verbose:
-        print("Joined input/output summary tables")
-
-    # Select one crop type
-    summary = summary.loc[summary["crop"] == CROP]
-    summary = summary.loc[summary["variety"] == VARIETY]
-    summary.drop(columns=["crop", "variety"], inplace=True)
-    if args.verbose:
-        print(f"Selected runs for {CROP}/{VARIETY}")
-
-    # Select one soil type
-    summary = summary.loc[summary["soiltype"] == SOILTYPE]
-    summary.drop(columns=["soiltype"], inplace=True)
-    if args.verbose:
-        print(f"Selected runs for {SOILTYPE} soil")
-
-    # Pre-process data
-    summary.to_crs(fpcup.geo.WGS84, inplace=True)
-    summary["latitude"], summary["longitude"] = summary["geometry"].y, summary["geometry"].x
