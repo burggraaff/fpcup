60f9f1054a3e360aec25b29afc259446ac169e77
Olivier Burggraaff <olivierburggraaff@gmail.com>
2024-09-18 12:29:04 +0200

    Better debug message

--- fpcup/__init__.py
+++ fpcup/__init__.py
@@ -4,3 +4,12 @@ from .model import run_pcse_single
 from .nn import dataset, network
 from .settings import DEFAULT_DATA, DEFAULT_OUTPUT, DEFAULT_RESULTS
 from .tools import RUNNING_IN_IPYTHON
+
+
+def test() -> None:
+    """
+    Simple function to assert the import was successful.
+    """
+    from importlib.metadata import version
+    module_version = version(__package__)
+    print(f"Succesfully imported {__package__} version {module_version}.")
